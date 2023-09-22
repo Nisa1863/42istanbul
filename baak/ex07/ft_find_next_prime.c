ab176d035fd6435a5dc066df8d0250454a73b676
Nisa Nur Özdal <116460949+Nisa1863@users.noreply.github.com>
2023-09-22 20:35:59 +0300

    Add files via upload

---
+++ baak/ex07/ft_find_next_prime.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nozdal <nozdal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 22:56:33 by nozdal            #+#    #+#             */
+/*   Updated: 2023/09/22 06:18:12 by nozdal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (2);
+	i = nb;
+	while (!ft_is_prime(i))
+		i++;
+	return (i);
+}
